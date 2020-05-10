81671c1168186a35e3463cfdd45da6994553704c
AymanAzzam <Ayman.Mohammed98@eng-st.cu.edu.eg>
2020-05-10 22:26:52 +0200

    Uploading run_app.sh file

---
+++ run_app.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Cloning the target repo for the app
+git clone https://github.com/dobromir-hristov/todo-app.git
+cd todo-app
+
+# Install packages
+npm install
+
+# Running unit tests
+yarn test:unit
+
+# Running e2e tests
+yarn test:e2e --headless
+
+# Start the app
+yarn serve --host $1 --disable-host-check  
